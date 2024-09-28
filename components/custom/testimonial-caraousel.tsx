5110ea5921ee7be5c3461de73b196aefc1cbfc53
shanv1337 <shanvit.shetty@iotric.com>
2024-09-28 23:19:41 +0530

    caraousel made more responsive

--- components/custom/testimonial-caraousel.tsx
+++ components/custom/testimonial-caraousel.tsx
@@ -1,6 +1,7 @@
 "use client";
-
+// HOOKS
 import { useState } from "react";
+// COMPONENTS
 import {
   Carousel,
   CarouselContent,
@@ -10,6 +11,7 @@ import {
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
 import Image, { StaticImageData } from "next/image";
+// ASSETS
 import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
 
 interface Item {
@@ -54,39 +56,48 @@ const TestimonialCarousel = ({ items }: { items: Item[] }) => {
               className={index === currentIndex ? "block" : "hidden"}
             >
               <Card className="border-none shadow-none">
-                <CardContent className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-0 min-h-96">
-                  <div className="relative h-full">
-                    <Image
-                      src={image}
-                      alt={name}
-                      className="absolute inset-0 object-cover"
-                    />
-                  </div>
-                  <div className="grid grid-cols-2 h-full">
-                    <p className="col-span-2 font-semibold text-2xl">
-                      {description}
-                    </p>
-
-                    <div className="col-span-1 space-y-2 whitespace-nowrap">
-                      <h2 className="font-bold text-3xl">
-                        {name},<span className="text-lg">{company}</span>
-                      </h2>
-                      <p className="font-semibold text-lg">{title}</p>
+                <CardContent className="p-4 sm:p-6 md:p-8">
+                  <div className="flex flex-col lg:flex-row lg:items-stretch min-h-[24rem]">
+                    <div className="w-full lg:w-1/2 aspect-square relative mb-6 lg:mb-0">
+                      <Image
+                        src={image}
+                        alt={name}
+                        className="absolute inset-0 w-full h-full object-cover"
+                      />
                     </div>
-
-                    <div className="col-span-1 place-self-end self-end flex h-full items-end gap-2">
-                      <CarouselPrevious
-                        onClick={handlePrevious}
-                        className="relative inset-auto rounded-full border border-black min-w-16 min-h-10"
-                      >
-                        <ArrowLeftIcon className="h-4 w-4" />
-                      </CarouselPrevious>
-                      <CarouselNext
-                        onClick={handleNext}
-                        className="relative inset-auto bg-primary text-white rounded-full min-w-16 min-h-10"
-                      >
-                        <ArrowRightIcon className="h-4 w-4" />
-                      </CarouselNext>
+                    <div className="w-full lg:w-1/2 lg:pl-8 flex flex-col justify-between">
+                      <p className="font-semibold text-lg sm:text-xl lg:text-2xl my-8">
+                        {description}
+                      </p>
+                      <div className="mt-auto">
+                        <div className="flex items-center justify-between">
+                          <div>
+                            <h2 className="font-bold text-xl sm:text-2xl lg:text-3xl">
+                              {name},
+                              <span className="text-base sm:text-lg lg:text-xl ml-2">
+                                {company}
+                              </span>
+                            </h2>
+                            <p className="font-semibold text-base sm:text-lg mt-2">
+                              {title}
+                            </p>
+                          </div>
+                          <div className="flex items-end gap-4 h-full">
+                            <CarouselPrevious
+                              onClick={handlePrevious}
+                              className="relative inset-auto rounded-full border border-black w-12 h-10 sm:w-16 sm:h-10"
+                            >
+                              <ArrowLeftIcon className="h-4 w-4" />
+                            </CarouselPrevious>
+                            <CarouselNext
+                              onClick={handleNext}
+                              className="relative inset-auto bg-primary text-white rounded-full w-12 h-10 sm:w-16 sm:h-10"
+                            >
+                              <ArrowRightIcon className="h-4 w-4" />
+                            </CarouselNext>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </CardContent>
